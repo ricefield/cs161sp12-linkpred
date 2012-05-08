368e325af42e52cb7acf5f3de20b430d9c36fd05
Yu Gan <frank.yugan@gmail.com>
2012-05-08 14:44:32 -0700

    better data, extract test stuff

--- features_util.py
+++ features_util.py
@@ -1,7 +1,7 @@
 import redis
 
 """
-Terminology: 
+Terminology:
     Interests - people who you've added in your circles
     Fans - people who've added you in their circles
     Friends - people who you've added and have added you in circles
@@ -11,60 +11,80 @@ class Feature_Extractor:
 
     """
     Params: snapshot is filename of snapshot as string
-    i.e for test-graph-2011-07-04.txt it will be 'test-graph-2011-07-04' 
+    i.e for test-graph-2011-07-04.txt it will be 'test-graph-2011-07-04'
     """
     def __init__(self, snapshot):
         self.snapshot = snapshot
         self.r_server=redis.Redis("localhost")
 
-    """ 
-    Returns number of users in the snapshot 
-    Params: None 
+    """
+    Returns number of users in the snapshot
+    Params: None
     """
     def num_users(self):
         return self.r_server.scard("%s:users" % (self.snapshot))
 
-    """ 
+    """
     Returns a set of all uid's in the snapshot
-    Params: None 
-    """ 
+    Params: None
+    """
     def get_users(self):
         return self.r_server.smembers("%s:users" % (self.snapshot))
 
-    """ 
+    """
     Returns a set of all uid's interests in the snapshot
-    Params: uid as a string 
-    """ 
+    Params: uid as a string
+    """
     def get_user_interests(self, uid):
         return self.r_server.smembers("%s:%s:interests" % (self.snapshot, uid))
 
 
-    """ 
+    """
     Returns a set of all uid's fans in the snapshot
-    Params: uid as a string 
-    """ 
+    Params: uid as a string
+    """
     def get_user_fans(self, uid):
         return self.r_server.smembers("%s:%s:fans" % (self.snapshot, uid))
 
 
-    """ 
+    """
     Returns a set of all uid's friends in the snapshot
-    Params: uid as a string 
-    """ 
+    Params: uid as a string
+    """
     def get_user_friends(self, uid):
         return self.r_server.sinter("%s:%s:interests" % (self.snapshot, uid), 
                                     "%s:%s:fans" % (self.snapshot, uid))
-   
-    """ 
-    Returns a set of the two users' common friends 
-    Params: uid1 and uid2 as strings 
+
+                                    
+    """
+    Returns a set of the two users' common friends
+    Params: uid1 and uid2 as strings
     """
     def get_common_friends(self, uid1, uid2):
         uid1_friends = self.get_user_friends(uid1)
         uid2_friends = self.get_user_friends(uid2)
-        return uid1_friends.intersection(uid2_friends) 
+        return uid1_friends.intersection(uid2_friends)
 
-def extract_values(set):
+        
+    """
+    Returns a list of tuples of test edges
+    """
+    def get_test_edges(self):
+        out = []
+        all_users = toList(self.get_users())
+        for user in all_users:
+            interests = self.get_user_interests(user)
+            fans = self.get_user_interests(user)
+            people = toList(interests.difference(fans))
+            for person in people:
+                out.append((user, person))
+        return out
+
+
+"""
+Extracts the values of a set into a list
+"""
+def toList(set):
     output = []
     while True:
         try:
